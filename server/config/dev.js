d7969ed63da92f9164bb80a33ace9a6543a3863c
Salman Rana <salman.r.rana@gmail.com>
2018-05-20 18:58:46 -0500

    added all the logic for billing

--- server/config/dev.js
+++ server/config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
     '551354662641-5e6qtkcostliclk02db4qj7r2l14ltcs.apps.googleusercontent.com',
   googleClientSecret: 'W1uFPP16YM7FhkBcPLIrFhA2',
   mongoURI: 'mongodb://salman:password@ds157599.mlab.com:57599/emaily-mern-dev',
-  cookieKey: 'biohdfjbhvsdkjfnbvkfhbvsizxhcv bsmfnbasdjlfbhvsdf'
+  cookieKey: 'biohdfjbhvsdkjfnbvkfhbvsizxhcv bsmfnbasdjlfbhvsdf',
+  stripePublishableKey: 'pk_test_2DGaXKsac6PtA396W8Vxfacw',
+  stripeSecretKey: 'sk_test_xBT3hPSp0FVTb9eXn6iaxa1j'
 };
